46e880e1a57ea3329277d1c111cd5ca6a89dc58e
MahaDay <daymaha2020@gmail.com>
2023-05-10 21:27:00 +0100

    first commit

---
+++ src/app/produit/details-produit/details-produit.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProduitService } from 'src/app/_services/produit.service';
+
+@Component({
+  selector: 'app-details-produit',
+  templateUrl: './details-produit.component.html',
+  styleUrls: ['./details-produit.component.css']
+})
+export class DetailsProduitComponent {
+  id:any
+  produit:any
+  constructor(private route:ActivatedRoute,private router: Router,
+     private produitService: ProduitService){
+
+  }
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    
+    this.produitService.getProduitById(this.id).subscribe(data=>{
+      this.produit = data;
+    });
+      
+  }
+  navigateToBackToList(){
+    this.router.navigate(['produits']);
+   }
+
+}
---
+++ src/app/transaction/details-transaction/details-transaction.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TransactionService } from 'src/app/_services/transaction.service';
+
+@Component({
+  selector: 'app-details-transaction',
+  templateUrl: './details-transaction.component.html',
+  styleUrls: ['./details-transaction.component.css']
+})
+export class DetailsTransactionComponent implements OnInit{
+  id:any
+  transaction:any
+  constructor(private route:ActivatedRoute,private router: Router,
+     private transactionService: TransactionService){
+
+  }
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    
+    
+    this.transactionService.getTransactionById(this.id).subscribe(data=>{
+      this.transaction = data;
+    });
+      
+  }
+
+  navigateToBackToList(){
+    this.router.navigate(['transactions']);
+   }
+  
+
+}
